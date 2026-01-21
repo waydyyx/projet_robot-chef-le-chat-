8e1badcdd45d85b50766ca17bc1b4d8625b2a972
waydyyx <phannguyendylan@gmail.com>
2026-01-21 12:29:47 +0100

    ajout du fichier main.py pour faire des tests et ajout du fichier class_robot.py avec les methode __init__ avancer tourner_droite

---
+++ sources/class_robot.py
@@ -0,0 +1,21 @@
+import math 
+
+class Robot:
+	def __init__(self,  angle : float, px : int = 0, py : int = 0):
+		self.px = px  # position x
+		self.py = py  # position y 
+		self.angle = angle
+		self.dx = math.cos(angle) * 5  # direction x (vecteur x ?)
+		self.dy = math.sin(angle) * 5  # direction y (vecteur y ?)
+	
+	def avancer(self):
+		self.px += self.dx
+		self.py += self.dy
+	
+	def tourner_droite(self):
+		self.angle += math.pi / 48
+		if (self.angle > 2 * math.pi):
+			self.angle -= 2 * math.pi
+		self.dx = math.cos(self.angle) * 5
+		self.dy = math.sin(self.angle) * 5
+		
---
+++ sources/main.py
@@ -0,0 +1,10 @@
+from class_robot import *
+
+def main():
+	test = Robot(0, 4, 4)
+	print(f"px : {test.px} py : {test.py}, angle : {test.angle}\n")
+
+if __name__ == "__main__":
+	main()
+
+
