16d80167cb9dd8b5897bcc961a65238de9279f2b
waydyyx <phannguyendylan@gmail.com>
2026-01-27 16:17:23 +0100

    ajout d'un fichier main.py qui lancer la simulation

--- sources/main.py
+++ sources/main.py
@@ -1,10 +1,7 @@
 from class_robot import *
-
-def main():
-	test = Robot(0, 4, 4)
-	print(f"px : {test.px} py : {test.py}, angle : {test.angle}\n")
+from simulation  import *
 
 if __name__ == "__main__":
-	main()
+	simulation()
 
 
--- sources/simulation.py
+++ sources/simulation.py
@@ -1,45 +1,47 @@
 import pygame
 from class_robot import *
-pygame.init()
 
-length = 800 
-width = 800
+def simulation():
+	pygame.init()
 
-screen = pygame.display.set_mode((length, width))
+	length = 800 
+	width = 800
 
-img_car_original = pygame.image.load("../images/robot_style.png").convert_alpha()
-img_car_length, img_car_width = img_car_original.get_size()
-print(f"length {img_car_length} width : {img_car_width}")
+	screen = pygame.display.set_mode((length, width))
 
-quit = 0
+	img_car_original = pygame.image.load("../images/robot_style.png").convert_alpha()
+	img_car_length, img_car_height = img_car_original.get_size()
+	print(f"length {img_car_length} width : {img_car_height}")
 
-clock = pygame.time.Clock()
-robot = Robot(0, 50, 50) # angle, coordonee de depart du robot (x,y)
-while not(quit):	
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
+	quit = 0
+
+	clock = pygame.time.Clock()
+	robot = Robot(0, 50, 50) # angle, coordonee de depart du robot (x,y)
+	while not(quit):	
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				quit = 1
+		pressed = pygame.key.get_pressed()
+		if (pressed[pygame.K_ESCAPE]):
 			quit = 1
-	pressed = pygame.key.get_pressed()
-	if (pressed[pygame.K_ESCAPE]):
-		quit = 1
-	if (pressed[pygame.K_UP] or pressed[pygame.K_w]):
-		if (robot.px <= length and robot.px >= 0 and robot.py < width and robot.py >= 0):
-			robot.avancer()
-		print(f"px :{robot.px} py :{robot.py}")
-	if (pressed[pygame.K_RIGHT] or pressed[pygame.K_d]):
-		robot.tourner_droite()
+		if (pressed[pygame.K_UP] or pressed[pygame.K_w]):
+			if (robot.px <= length and robot.px >= 0 and robot.py < width and robot.py >= 0):
+				robot.avancer()
+			print(f"px :{robot.px} py :{robot.py}")
+		if (pressed[pygame.K_RIGHT] or pressed[pygame.K_d]):
+			robot.tourner_droite()
 
-	if (pressed[pygame.K_DOWN] or pressed[pygame.K_s]):
-		robot.reculer()
-	if (pressed[pygame.K_LEFT] or pressed[pygame.K_a]):
-		robot.tourner_gauche()
+		if (pressed[pygame.K_DOWN] or pressed[pygame.K_s]):
+			robot.reculer()
+		if (pressed[pygame.K_LEFT] or pressed[pygame.K_a]):
+			robot.tourner_gauche()
 
-	
-	screen.fill((255, 255, 255))
-	img_car_rotation = pygame.transform.rotate(img_car_original, -math.degrees(robot.angle) + 90)
-	# print(math.degrees(robot.angle))
-	rect=img_car_rotation.get_rect(center=((robot.px+img_car_length/2),(robot.py+img_car_width/2)))
-	screen.blit(img_car_rotation,rect)
-	pygame.display.flip()
-	clock.tick(60)
-pygame.quit()
+
+		screen.fill((255, 255, 255))
+		img_car_rotation = pygame.transform.rotate(img_car_original, -math.degrees(robot.angle) + 90)
+		# print(math.degrees(robot.angle))
+		rect=img_car_rotation.get_rect(center=((robot.px + img_car_length / 2),(robot.py + img_car_height / 2)))
+		screen.blit(img_car_rotation,rect)
+		pygame.display.flip()
+		clock.tick(60)
+	pygame.quit()
