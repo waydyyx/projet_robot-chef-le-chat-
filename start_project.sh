ace9411744256b53a5392b3cbc74e80c739df5cb
waydyyx <phannguyendylan@gmail.com>
2026-02-05 11:47:12 +0100

    suppression de start_project.sh et d'un fichier test

--- start_project.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-[ $# -ne 5 ] && echo "Veuillez enter 5 arguments, vitesse ([int] 0-10), vitesse_rotation ([int] 0-10), l'angle ([int] 0-359), position x ([int]), position y ([int])" && exit 1
-cd sources
-python3 main.py $1 $2 $3 $4 $5
-cd ..
