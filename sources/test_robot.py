77376a12faf0bb2e333b5f2c2d4147d64727f810
Titouan Laville <titouanlaville1@gmail.com>
2026-02-24 23:54:40 +0100

    ajout de test_robot.py et de la fonction test_avancer

---
+++ sources/test_robot.py
@@ -0,0 +1,17 @@
+from robot import Robot
+import unittest
+import math 
+
+class test_robot(unittest.TestCase):
+    def setUp(self):
+        self.robot=Robot(5,5,0,100,100)
+
+    def test_avancer(self):
+        x=self.robot.px
+        y=self.robot.py
+        angle=self.robot.angle
+        self.robot.avancer()
+        self.assertEqual(self.robot.px,x+((self.robot.vitesse_d + self.robot.vitesse_g) / 2 * math.cos(angle + (self.robot.vitesse_g - self.robot.vitesse_d) / self.robot.L)))
+        self.assertEqual(self.robot.py,y+((self.robot.vitesse_d + self.robot.vitesse_g) / 2 * math.sin(angle + (self.robot.vitesse_g - self.robot.vitesse_d) / self.robot.L)))
+        self.assertEqual(self.robot.angle,(angle+(self.robot.vitesse_g - self.robot.vitesse_d) / self.robot.L)% (2 * math.pi))
+
