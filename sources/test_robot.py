0a4834f9e47c9268612439c000e9b5f6e645bc5e
guillaumedong5235-create <guillaumedong5235@gmail.com>
2026-02-25 01:14:42 +0100

    test_robot.py: ajout de la fonction test_change_vitesse

--- sources/test_robot.py
+++ sources/test_robot.py
@@ -15,3 +15,24 @@ class test_robot(unittest.TestCase):
         self.assertEqual(self.robot.py,y+((self.robot.vitesse_d + self.robot.vitesse_g) / 2 * math.sin(angle + (self.robot.vitesse_g - self.robot.vitesse_d) / self.robot.L)))
         self.assertEqual(self.robot.angle,(angle+(self.robot.vitesse_g - self.robot.vitesse_d) / self.robot.L)% (2 * math.pi))
 
+
+
+    #def test_tourner_droite(self):
+    #    self.robot.tourner_droite(90)
+    #    angle_rechercher = math.pi/2
+
+    def test_change_vitesse(self):
+        self.robot.change_vitesse(5, -3)
+        self.assertEqual(self.robot.vitesse_g, 5)
+        self.assertEqual(self.robot.vitesse_d, -3)
+        self.robot.change_vitesse(20, 15)
+        self.assertEqual(self.robot.vitesse_g, 10)
+        self.assertEqual(self.robot.vitesse_d, 10)
+        self.robot.change_vitesse(-20, -15)
+        self.assertEqual(self.robot.vitesse_g, -10)
+        self.assertEqual(self.robot.vitesse_d, -10)
+        self.robot.change_vitesse(20, -20)
+        self.assertEqual(self.robot.vitesse_g, 10)
+        self.assertEqual(self.robot.vitesse_d, -10)
+
+
