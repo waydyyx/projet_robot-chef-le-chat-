e6e34f9f96f958a901bed5561fbad0f42283f535
Titouan Laville <titouanlaville1@gmail.com>
2026-03-18 10:10:02 +0100

    ajout des lock dans autonomme rectangle et update_modele

--- sources/modele/update_modele.py
+++ sources/modele/update_modele.py
@@ -4,8 +4,8 @@ def update_mod(arene:Arene):
     while True:
         if (arene.collision_bord() or arene.collision_obstacle()):
             with arene.stop_lock:
-                    arene.stop = 1
-                    return
+                arene.stop = 1
+                return
         with arene.stop_lock:
             if arene.stop==1:
                 return
--- sources/strategie/rectangle.py
+++ sources/strategie/rectangle.py
@@ -8,9 +8,11 @@ def rectangle(arene: Arene, longeur: int, hauteur: int, vitesse: int):
     d = arene.robot.vitesse_d 
     g = arene.robot.vitesse_g
     for i in range (2):
-        arene.robot.strat_avancer(longeur,vitesse)
-        arene.robot.tourner_droite(90)
-        arene.robot.strat_avancer(hauteur,vitesse)
-        arene.robot.tourner_droite(90)
-    arene.robot.change_vitesse(g,d)
+        with arene.robot.lock:
+            arene.robot.strat_avancer(longeur,vitesse)
+            arene.robot.tourner_droite(90)
+            arene.robot.strat_avancer(hauteur,vitesse)
+            arene.robot.tourner_droite(90)
+    with arene.robot.lock:
+        arene.robot.change_vitesse(g,d)
     return 
