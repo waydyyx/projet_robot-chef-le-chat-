053c94faf1494ddfd9291e064394886cb4090772
Titouan Laville <titouanlaville1@gmail.com>
2026-04-08 09:39:11 +0200

    q1.2

--- sources/view/affichage.py
+++ sources/view/affichage.py
@@ -16,14 +16,19 @@ class Affichage :
         self.arene=arene
 
     def affiche(self):
+        coordonne=[]
         while (True):
             self.screen.fill((255, 255, 255))
             self.affiche_obstacle()
             with self.arene.robot.lock:
                 img_robot_rotation = pygame.transform.rotate(self.img_robot, -math.degrees(self.arene.robot.angle) + 90)
                 rect = img_robot_rotation.get_rect(center=((self.arene.robot.px + self.img_robot_larg / 2),(self.arene.robot.py + self.img_robot_haut / 2)))
+                if self.arene.robot.dessine:
+                    coordonne.append((int(self.arene.robot.px+self.img_robot_larg/2),int(self.arene.robot.py+self.img_robot_haut/2)))
             self.screen.blit(img_robot_rotation, rect)  
             #pygame.gfxdraw.pixel(self.screen,int(self.arene.robot.px),int(self.arene.robot.py),(0, 0, 255))
+            for i in coordonne:
+                pygame.gfxdraw.pixel(self.screen,i[0],i[1],(0,0,255))
             pygame.display.flip()
             with self.arene.stop_lock:
                 if self.arene.stop == 1:
@@ -34,6 +39,7 @@ class Affichage :
         clock = pygame.time.Clock()
         # Lecture des touches
         while not(self.arene.stop):
+            
             for event in pygame.event.get():
                 if (event.type == pygame.QUIT):
                     traiter_touche(self.arene, "QUIT")
