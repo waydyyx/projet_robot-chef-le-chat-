ee7cd7359e166764b443d41148708aaec5bac4b4
Titouan Laville <titouanlaville1@gmail.com>
2026-05-04 00:07:50 +0200

    cleanup de state

--- sources/traducteur/state.py
+++ sources/traducteur/state.py
@@ -3,38 +3,14 @@ from threading import Thread
 from modele.update_modele import update_mod
 # from API_robot.robot2I013 import Robot2IN013
 
-# class Simulation:
-#     def __init__(self, st):
-#         self._st=st
-    
-#     def start(self):
-#         for event in pygame.event.get():
-#                 pass
-#         Thread(target=update_mod,args=(self._st.arene,)).start()
-#         # Thread(target=self._st.affiche).start()
-#         self._st.affiche()
-            
-
-    # def __getattribute__(self, name):
-    #     return getattr(self._st, name)$
-
-SEUIL_DETECTION_MM = 300  # 30 cm
-
 class Robot_IRL:
-    def __init__(self,robot:"Robot2IN013"):
+    def __init__(self, robot):
         self.robot=robot
         #self.robot.ray=33.25
-
-    def start(self):
-        print("yeaah")
 
     def change_vitesse(self,vit_g,vit_d):
         self.robot.set_motor_dps("MOTOR_LEFT", vit_g)
         self.robot.set_motor_dps("MOTOR_RIGHT", vit_d)
-
-    #  def change_vitesse(self,vit_g,vit_d):
-    #     self.robot.set_motor_dps(Robot2IN013.MOTOR_LEFT, vit_g)
-    #     self.robot.set_motor_dps(Robot2IN013.MOTOR_RIGHT, vit_d)
 
     def stop(self):
         self.robot.stop()
@@ -45,16 +21,9 @@ class Robot_IRL:
     def set_rot(self,offset,port):
         self.robot.offset_motor_encoder(port,offset)
 
-
-    #def set_rot(self, offset, roues: str = "both"):
-    #    if roues == "L" or roues == "both":
-    #        current_g, _ = self.robot.get_motor_position()
-    #       self.robot.offset_motor_encoder(Robot2IN013.MOTOR_LEFT, current_g - offset)
-    #    if roues == "R" or roues == "both":
-    #        _, current_d = self.robot.get_motor_position()
-    #       self.robot.offset_motor_encoder(Robot2IN013.MOTOR_RIGHT, current_d - offset)
-
-
+    def get_distance(self):
+        return self.robot.get_distance()
+    
     def get_distance(self):
         return self.robot.get_distance()
     
